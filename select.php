062d650158af3efedb49b8e7be602a00aaba1f3a
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-10-21 10:18:43 -0500

    select con formato de tabla

--- select.php
+++ select.php
@@ -2,15 +2,30 @@
 include_once ('connect.php');
 
     $sql = 'SELECT code, name, continent, region FROM [world].[country] ORDER BY name';
+   $query = $conn -> prepare($sql); 
+  $query -> execute(); 
+$results = $query -> fetchAll(PDO::FETCH_OBJ); 
 
-    foreach ($conn->query($sql) as $row) {
-        echo $row['code'] . " | ";
-        echo $row['name'] . " | ";
-        echo $row['continent'] . " | ";
-        echo $row['region'] . " | ";
+    if($query -> rowCount() > 0)   { 
+    echo "<table border='1px'>";
+    echo "<tr>
+      <td>Name</td>
+      <td>Continent</td>
+      <td>Region</td>
+      <td>Population</td>
+    </tr>";
+    foreach($results as $result) { 
+      echo "<tr>
+        <td>" . $result->Name . "</td>
+        <td>" . $result->Continent . "</td>
+        <td>" . $result->Region . "</td>
+        <td>" . $result->Population . "</td>
+      </tr>";
     }
+    echo "</table>";
+  }
 
     // use exec() because no results are returned
-    $conn->exec($sql);
+    
 
 $conn = null;
