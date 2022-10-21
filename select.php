9cd1c37a53880741bdfadaf320871e294ed176e1
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-10-20 22:35:39 -0500

    1.2 select de consulta base de datos world

--- select.php
+++ select.php
@@ -1,7 +1,7 @@
 <?php
 include_once ('connect.php');
 
-    $sql = 'SELECT code, name, continent, region, FROM Country ORDER BY name';
+    $sql = 'SELECT code, name, continent, region FROM [world].[country] ORDER BY name';
 
     foreach ($conn->query($sql) as $row) {
         echo $row['code'] . " | ";
