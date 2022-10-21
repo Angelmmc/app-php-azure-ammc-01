d0e11d17dfb0c8840230f234b688ea4d9f342408
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-10-20 22:30:37 -0500

    Select aplicado a base de datos world

--- select.php
+++ select.php
@@ -1,14 +1,13 @@
 <?php
 include_once ('connect.php');
 
-    $sql = 'SELECT id, firstname, lastname, email, reg_date FROM MyGuests ORDER BY lastname';
+    $sql = 'SELECT code, name, continent, region, FROM Country ORDER BY name';
 
     foreach ($conn->query($sql) as $row) {
-        echo $row['id'] . " | ";
-        echo $row['firstname'] . " | ";
-        echo $row['lastname'] . " | ";
-        echo $row['email'] . " | ";
-        echo $row['reg_date'] . "<br>";
+        echo $row['code'] . " | ";
+        echo $row['name'] . " | ";
+        echo $row['continent'] . " | ";
+        echo $row['region'] . " | ";
     }
 
     // use exec() because no results are returned
