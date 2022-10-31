e45e2d2716eb87f40e19a5cda9c7690eea999545
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-10-31 16:27:38 -0600

    API v1
    API proporcionando el dato de forma directa

---
+++ php-ip-api/select.php
@@ -0,0 +1,35 @@
+<?php
+include ('connect.php');
+
+$code = 'ES';
+  
+$sql = "select world.country.name 'País', continent 'Continente', region 'Region',world.city.name 'Capital', code, code2 from [world].[country]
+inner join world.city on world.country.capital=world.city.id
+where code2='".$code."'"; 
+$query = $conn -> prepare($sql); 
+$query -> execute(); 
+$results = $query -> fetchAll(PDO::FETCH_OBJ); 
+
+if($query -> rowCount() > 0)   { 
+  echo "<table border='1px', padding='5px'>";
+  echo "<tr>
+    <td>País</td>
+    <td>Capital</td>
+    <td>Continente</td>
+    <td>Region</td>
+    <td>Code</td>
+    <td>Code 2</td>
+  </tr>";
+  foreach($results as $result) { 
+    echo "<tr>
+      <td>" . $result->Pais . "</td>
+      <td>" . $result->Capital . "</td>
+      <td>" . $result->Continente . "</td>
+      <td>" . $result->Region . "</td>
+      <td>" . $result->code . "</td>
+      <td>" . $result->code2 . "</td>
+    </tr>";
+  }
+  echo "</table>";
+}
+?>
