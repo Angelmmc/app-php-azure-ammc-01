97ba9d409459a6ff97c5b7c350469f99ba8bc37a
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-10-31 20:01:19 -0600

    Update connect.php

--- php-ip-api/connect.php
+++ php-ip-api/connect.php
@@ -9,7 +9,7 @@ try {
     $conn = new PDO("sqlsrv:server = $serverName,$port; Database = $database", $userName, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "*";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
