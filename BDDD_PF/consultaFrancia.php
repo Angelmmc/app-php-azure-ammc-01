41a5ba8d7232976c3931194fed7688301f8d45c1
Ángel Manuel Medina Cárdenas <64828944+angeltacos28@users.noreply.github.com>
2022-12-06 18:23:34 -0600

    francia frutas

--- BDDD_PF/consultaFrancia.php
+++ BDDD_PF/consultaFrancia.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 include("conexiones/conexion_francia.php");
 
-$sql1 = "select * from episodios1"; 
+$sql = "select * from frutas1"; 
 
 $query1 = $conn1 -> prepare($sql1); 
 $query1 -> execute();  
@@ -12,14 +12,28 @@ if($query1 -> rowCount() > 0)   {
   echo "<table border='1px', padding='5px'>";
   echo "<tr>
     <td>ID</td>
-    <td>Capitulo</td>
-    <td>Temporada</td>
+    <td>Nombre</td>
+    <td>Genero</td>
+    <td>Familia</td>
+    <td>Orden</td>
+    <td>Carbohidratos (100g)</td>
+    <td>Proteinas (100g)</td>
+    <td>Grasas (100g)</td>
+    <td>Calorias (100g)</td>
+    <td>Azucar (100g)</td>
   </tr>";
   foreach($results as $result) { 
     echo "<tr>
       <td>" . $result->id . "</td>
       <td>" . $result->nombre . "</td>
-      <td>" . $result->temporada . "</td>
+      <td>" . $result->genero . "</td>
+      <td>" . $result->familia . "</td>
+      <td>" . $result->orden . "</td>
+      <td>" . $result->carbohidratos . "</td>
+      <td>" . $result->proteinas . "</td>
+      <td>" . $result->grasas . "</td>
+      <td>" . $result->calorias . "</td>
+      <td>" . $result->azucar . "</td>
     </tr>"; 
   }
   echo "</table>";
